92de5d277343811c520f39356d82c0e7ffad795a
ELOUSSE-ABDELMAJID <lord.of.the.ring@outlook.com>
2022-09-07 15:03:17 +0200

    function

---
+++ challenge5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void    function_sensation(int cel)
+{
+    if (cel <= 0)
+        printf("\nTres Froid \n");
+    else if (cel < 25)
+        printf("\nFroid \n");
+    else if (cel >= 25 && cel < 45)
+        printf("\nChaud\n");
+    else
+        printf("\nTres Chaud \n");
+}
+
+int main()
+{
+	int fah;
+    int cel;
+
+	printf("--> Donner la température en Fahrenheit :\n\n");
+	scanf("%d", &fah);
+    cel = (fah - 32) * 5/9;
+    function_sensation(cel);
+	return (0);
+}
