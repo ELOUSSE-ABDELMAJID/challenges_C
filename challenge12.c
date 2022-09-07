64acb071d9c852ce5d31ec78ad4d38b0b5214423
ELOUSSE-ABDELMAJID <lord.of.the.ring@outlook.com>
2022-09-07 18:26:08 +0200

    challenge 12

--- challenge12.c
+++ challenge12.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-	int *nbr;
+	char nbr[3];
 
     printf("Entrez un nombre de trois chiffre : \n");
-    scanf("%d",nbr);
-    printf("Le nombre inversee est : %d%d%d\n", nbr[2],nbr[1],nbr[0]);
+    scanf("%s",nbr);
+    printf("Le nombre inversee est : %c%c%c\n", nbr[2],nbr[1],nbr[0]);
 	return (0);
 }
