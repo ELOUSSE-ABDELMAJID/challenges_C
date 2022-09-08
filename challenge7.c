356ffe1f6acef65ffb1fd5c1e36d60a9642575e6
ELOUSSE-ABDELMAJID <lord.of.the.ring@outlook.com>
2022-09-08 10:55:21 +0200

    good morning

--- challenge7.c
+++ challenge7.c
@@ -3,16 +3,13 @@
 int main()
 {
 	int a, b;
-    
+
     printf("Entrez le variable a et b : \n");
-    scanf("%d \t %d", &a,&b);
+    scanf("%d  %d", &a,&b);
     printf("La Somme : %d \n", a+b);
     printf("La Soustraction : %d\n", a-b);
     printf("La Multiplication : %d\n", a*b);
-    if (b != 0)
-        printf("La Division : %d\n", a/b);
-    else
-        printf("La Division est Impossible");
     printf("Le Modulo : %d\n", a%b);
+    printf("La Division : %d\n", a/b);
 	return (0);
 }
