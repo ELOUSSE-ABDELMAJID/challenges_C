3f5205143d8fc9e13dbcb9db7812e18cf6e21967
ELOUSSE-ABDELMAJID <lord.of.the.ring@outlook.com>
2022-09-08 14:36:21 +0200

    challenge 2

---
+++ Workshop2/challenge2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+
+    printf("Enter a Character : \n");
+    scanf("%c", &c);
+    switch (c)
+    {
+    case 'a':
+        printf("This Character is a voyalle.\n");
+        break;
+    case 'e':
+        printf("This Character is a voyalle.\n");
+        break;
+    case 'i':
+        printf("This Character is a voyalle.\n");
+        break;
+    case 'o':
+        printf("This Character is a voyalle.\n");
+        break;
+    case 'u':
+        printf("This Character is a voyalle.\n");
+        break;
+    case 'y':
+        printf("This Character is a voyalle.\n");
+        break;
+    default:
+        printf("This Character is not a voyalle.\n");
+    }
+    return (0);
+}
