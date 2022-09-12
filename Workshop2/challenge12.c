60511e1e48717acc3b595d03485e02bde6a67de2
ELOUSSE-ABDELMAJID <lord.of.the.ring@outlook.com>
2022-09-12 16:20:46 +0200

    challenge 12

--- Workshop2/challenge12.c
+++ Workshop2/challenge12.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char    *copy(char *dest, char *src)
 {
     int i = 0;
+    int len = 0;
 
+    while (src[len])
+        len++;
+    dest = malloc(sizeof(char) * (len + 1));
     while (src[i])
     {
         dest[i] = src[i];
         i++;
     }
+    dest[i] = '\0';
     return (dest);
 }
 
@@ -19,6 +26,6 @@ int main()
     printf("Enter a word : ");
     scanf("%s", src);
     dest = copy(dest, src);
-    printf("--> %s\n", dest);
+    printf("%s\n", dest);
     return (0);
 }
